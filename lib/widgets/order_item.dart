9feb6bdd6f03f2253c0bbe06498caa3ca5d176bb
Basith P <basithpofficial@gmail.com>
2021-12-12 22:07:54 +0530

    Create orders screen

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../provider/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  const OrderItem({Key? key, required this.order}) : super(key: key);
+
+  final ord.OrderItem order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Rs. ${order.amount}'),
+            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(order.dateTime)),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.expand_more_rounded),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
