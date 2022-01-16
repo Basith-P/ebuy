c18c01bdd5144814344c3d5e39da726b352b6e57
Basith P <basithpofficial@gmail.com>
2022-01-16 14:37:55 +0530

    Change orders to future builder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -13,40 +13,47 @@ class OrdersPage extends StatefulWidget {
 }
 
 class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = false;
+  late Future _ordersFuture;
 
-  //you can also use didChangeDependencies like in the productspage
+  Future _obtainOrdersFuture() {
+    return context.read<Orders>().fetchAndSetOrders();
+  }
+
   @override
   void initState() {
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await context.read<Orders>().fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      drawer: const LeftDrawer(),
-      appBar: AppBar(
-        title: const Text('Your Orders'),
-      ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator(strokeWidth: 2))
-          : ListView.builder(
-              padding: const EdgeInsets.all(15),
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(
-                order: orderData.orders[i],
-              ),
-            ),
-    );
+        drawer: const LeftDrawer(),
+        appBar: AppBar(
+          title: const Text('Your Orders'),
+        ),
+        body: FutureBuilder(
+          future: _ordersFuture,
+          builder: (ctx, snapShot) {
+            if (snapShot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+            } else {
+              if (snapShot.hasError) {
+                //...
+                return const Center(child: Text('An error occured'));
+              } else {
+                return Consumer<Orders>(
+                  builder: ((ctx, orderData, child) => ListView.builder(
+                        padding: const EdgeInsets.all(15),
+                        itemCount: orderData.orders.length,
+                        itemBuilder: (ctx, i) => OrderItem(
+                          order: orderData.orders[i],
+                        ),
+                      )),
+                );
+              }
+            }
+          },
+        ));
   }
 }
